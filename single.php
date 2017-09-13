7d87a70d53cb2c03139b5c092ce024fef61de741
fchristiansen <felipe@seo2.cl>
2017-09-13 13:54:03 -0300

    desarrollo wp

--- single.php
+++ single.php
@@ -1,21 +1,22 @@
 <?php
 if (in_category('2')) {
 	include (TEMPLATEPATH . '/single-entrevista.php');
-}
-$args = array (
-  'post_type' => 'post',
-  'posts_per_page' => 1
-);
-$the_query = new WP_Query ($args);
-if ( have_posts() ) : while ( $the_query->have_posts() ) : $the_query->the_post();
-      if(get('plantilla_tipo_de_plantilla')=='Con sidebar'){
-         include (TEMPLATEPATH . '/single-con-barra.php');
-      }else{
-        include (TEMPLATEPATH . '/single-sin-barra.php');
-      }
-    endwhile; else:
-    endif;
-    wp_reset_postdata();
-?>
+}else{
+  $args = array (
+    'post_type' => 'post',
+    'posts_per_page' => 1
+  );
+  $the_query = new WP_Query ($args);
+  if ( have_posts() ) : while ( $the_query->have_posts() ) : $the_query->the_post();
+        if(get('plantilla_tipo_de_plantilla')=='Con sidebar'){
+           include (TEMPLATEPATH . '/single-con-barra.php');
+        }elseif(get('plantilla_tipo_de_plantilla')=='Sin sidebar'){
+          include (TEMPLATEPATH . '/single-sin-barra.php');
+        }
+      endwhile; else:
+      endif;
+      wp_reset_postdata();
+
+}?>
 
 
