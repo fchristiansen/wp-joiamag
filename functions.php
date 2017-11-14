67f55c93a2ea34e2440394d1289a52ad1a6208e2
Carolina Rojas <carorojas27@gmail.com>
2017-11-14 11:15:33 -0300

    upload functions
    agrega funcion para menu

--- functions.php
+++ functions.php
@@ -259,7 +259,64 @@ if (class_exists('MultiPostThumbnails')) {
         }
 ////////////////////////////////////
 
-
+function clean_custom_menu( $theme_location ) {
+    if ( ($theme_location) && ($locations = get_nav_menu_locations()) && isset($locations[$theme_location]) ) {
+        $menu = get_term( $locations[$theme_location], 'nav_menu' );
+        $menu_items = wp_get_nav_menu_items($menu->term_id);
+ 
+        $menu_list  = '<nav>' ."\n";
+        $menu_list .= '<ul class="main-nav">' ."\n";
+ 
+        $count = 0;
+        $submenu = false;
+         
+        foreach( $menu_items as $menu_item ) {
+             
+            $link = $menu_item->url;
+            $title = $menu_item->title;
+             
+            if ( !$menu_item->menu_item_parent ) {
+                $parent_id = $menu_item->ID;
+                 
+                $menu_list .= '<li class="item">' ."\n";
+                $menu_list .= '<a href="'.$link.'" class="title">'.$title.'</a>' ."\n";
+            }
+ 
+            if ( $parent_id == $menu_item->menu_item_parent ) {
+ 
+                if ( !$submenu ) {
+                    $submenu = true;
+                    $menu_list .= '<ul class="sub-menu">' ."\n";
+                }
+ 
+                $menu_list .= '<li class="item">' ."\n";
+                $menu_list .= '<a href="'.$link.'" class="title">'.$title.'</a>' ."\n";
+                $menu_list .= '</li>' ."\n";
+                     
+ 
+                if ( $menu_items[ $count + 1 ]->menu_item_parent != $parent_id && $submenu ){
+                    $menu_list .= '</ul>' ."\n";
+                    $submenu = false;
+                }
+ 
+            }
+ 
+            if ( $menu_items[ $count + 1 ]->menu_item_parent != $parent_id ) { 
+                $menu_list .= '</li>' ."\n";      
+                $submenu = false;
+            }
+ 
+            $count++;
+        }
+         
+        $menu_list .= '</ul>' ."\n";
+        $menu_list .= '</nav>' ."\n";
+ 
+    } else {
+        $menu_list = '<!-- no menu defined in location "'.$theme_location.'" -->';
+    }
+    echo $menu_list;
+}
 
 
 ?>
