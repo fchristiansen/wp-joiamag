25547974490a15e40955ec4391e093ea584c8dab
Carolina Rojas <carorojas27@gmail.com>
2017-11-14 11:22:21 -0300

    custom menu

--- functions.php
+++ functions.php
@@ -318,5 +318,14 @@ function clean_custom_menu( $theme_location ) {
     echo $menu_list;
 }
 
+function wpb_custom_new_menu() {
+  register_nav_menus(
+    array(
+      'my-custom-menu' => __( 'My Custom Menu' ),
+      'extra-menu' => __( 'Extra Menu' )
+    )
+  );
+}
+add_action( 'init', 'wpb_custom_new_menu' );
 
 ?>
