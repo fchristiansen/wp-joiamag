dd569302eb4cb3ac1fa313e4094f151a8c04f857
CaroRed <carorojas27@gmail.com>
2018-01-05 10:22:29 -0300

    cambios checkout pago transferencia

--- woocommerce/cmsms-woo-functions.php
+++ woocommerce/cmsms-woo-functions.php
@@ -326,3 +326,27 @@ function sww_change_wc_currency_symbol( $currency_symbol, $currency ) {
 }
 add_filter('woocommerce_currency_symbol', 'sww_change_wc_currency_symbol', 10, 2);
 
+/**
+* filter translations, to replace some WooCommerce text with our own
+* @param string $translation the translated text
+* @param string $text the text before translation
+* @param string $domain the gettext domain for translation
+* @return string
+*/
+function wpse_77783_woo_bacs_ibn($translation, $text, $domain) {
+    if ($domain == 'woocommerce') {
+        switch ($text) {
+            case 'IBAN':
+                $translation = 'RUT';
+                break;
+
+            case 'Account number':
+                $translation = 'Cuenta Corriente';
+                break;
+        }
+    }
+
+    return $translation;
+}
+
+add_filter('gettext', 'wpse_77783_woo_bacs_ibn', 10, 3);
